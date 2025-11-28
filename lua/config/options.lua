c597e7aaad3565637dec7f17a87f87a52043689a
mcreinii <mcreinii@gmail.com>
2025-11-28 13:53:55 +0800

    Neovide support.

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,4 +1,5 @@
 local opt = vim.o
+local g = vim.g
 
 opt.tabstop = 4
 opt.shiftwidth = 4
@@ -9,3 +10,11 @@ opt.wrap = true
 opt.autoindent = true
 opt.clipboard = "unnamed"
 opt.termguicolors = true
+
+if vim.g.neovide then
+	opt.guifont = "MesloLGM Nerd Font Mono,JetBrainsMono Nerd Font:h10"
+	g.neovide_opacity = 0.8
+	g.neovide_normal_opacity = 0.8
+	g.neovide_hide_mouse_when_typing = true
+	vim.notify("Using Neovide.\nCertain options are enabled.", vim.log.levels.INFO)
+end
